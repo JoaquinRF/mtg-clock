6c7a3c9b28cba2673d1bd86b2f6bf65418fbb717
Joaquin R Fajardo <jfajardo@icstars.org>
2022-12-04 23:09:08 -0600

    testing

--- src/components/PriorityIndicator.tsx
+++ src/components/PriorityIndicator.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 type Props = {}
-
+// Testing PRs
 function PriorityIndicator({ }: Props) {
     return (
         <div className='border w-fit p-4 ml-4 mt-4'>Red player has priority</div>
